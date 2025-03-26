f9288c5cf198dcb6914343db75e0e8417de97fc2
JustJanGG <jan.haufer@gmail.com>
2025-03-26 20:31:44 +0100

    basic fireball functionality

---
+++ Assets/Scripts/Placeholder/ShootFiireball.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootFireball : MonoBehaviour
+{
+    private FireballProto fireballProto;
+    public GameObject fireballPrefab;
+
+    private float lastFireballTime = 0.0f;
+    private void Start()
+    {
+        fireballProto = fireballPrefab.GetComponent<FireballProto>();
+    }
+    public void Shoot(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && Time.time > fireballProto.castDelay + lastFireballTime)
+        {
+            Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+            lastFireballTime = Time.time;
+        }
+    }
+
+}
