4cfd195725d9bc7a4b385af7bc56374c66f0aa9b
tlpetrick <lpetrick@stud.hs-heilbronn.de>
2025-04-03 14:39:06 +0200

    simplify movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,10 @@ public class PlayerController : MonoBehaviour
     [Header("Components")]
     private Rigidbody2D rigidBody;
     private Collider2D playerCollider;
-
+    
     [Header("Movement")]
     private Vector2 direction;
     public float movementSpeed = 9f;
-    public float acceleration = 7f;
-    public float decceleration = 9f;
-    public float velPower = 1.2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,34 +18,18 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        MoveRight();
-        MoveUp();
-    }
-    private void MoveRight()
-    {
-        float targetSpeed = direction.x * movementSpeed;
-        float speedDif = targetSpeed - rigidBody.linearVelocity.x;
-        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
-        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
-
-        rigidBody.AddForce(movement * Vector2.right, ForceMode2D.Force);
+        Move();
     }
 
-    private void MoveUp()
+    private void Move()
     {
-        float targetSpeed = direction.y * movementSpeed;
-        float speedDif = targetSpeed - rigidBody.linearVelocity.y;
-        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
-        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
-
-        rigidBody.AddForce(movement * Vector2.up, ForceMode2D.Force);
+        rigidBody.linearVelocity = new Vector2(direction.x * movementSpeed, direction.y * movementSpeed);
     }
-
 
     public void HandleMoveInput(InputAction.CallbackContext ctx)
     {
-        direction = ctx.ReadValue<Vector2>();
+        direction = ctx.ReadValue<Vector2>().normalized;
     }
 }
