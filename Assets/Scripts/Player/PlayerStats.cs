076b576197280a24a13384454d064d041a6f6fee
tlpetrick <lpetrick@stud.hs-heilbronn.de>
2025-04-07 18:20:34 +0200

    add first stats

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Stats")]
+    public int health = 3;
+    public int maxHealth = 3;
+    public float healthRegen;
+    public float movementSpeed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
