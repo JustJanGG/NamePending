8d16ce311699d6979e50b7d3767d58b2ac4ff92c
tlpetrick <lpetrick@stud.hs-heilbronn.de>
2025-04-10 14:17:34 +0200

    small tag changes

--- Assets/Scripts/Enemy/Placeholder/Slime/SlimeMovement.cs
+++ Assets/Scripts/Enemy/Placeholder/Slime/SlimeMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SlimeMovement : MonoBehaviour
@@ -11,31 +12,57 @@ public class SlimeMovement : MonoBehaviour
     public GameObject player;
     private float distanceToPlayer;
     public Vector2 direction;
+    //private bool isEvading;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        //isEvading = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movementSpeed = GetComponent<SlimeStats>().movementSpeed;
-        aggroRange = GetComponent<SlimeStats>().aggroRange;
-
-        distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-        direction = player.transform.position - transform.position;
-        direction.Normalize();
-
-        if (distanceToPlayer < aggroRange)
-        {
-            rigidBody.linearVelocity = new Vector2(direction.x * movementSpeed, direction.y * movementSpeed);
-        }
-        else
-        {
-            rigidBody.linearVelocity = new Vector2(0, 0);
-            direction = Vector2.zero;
-        }
+        Move();
     }
+
+    private void Move()
+    {
+        //if (!isEvading)
+        //{
+            movementSpeed = GetComponent<SlimeStats>().movementSpeed;
+            aggroRange = GetComponent<SlimeStats>().aggroRange;
+
+            distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+            direction = player.transform.position - transform.position;
+            direction.Normalize();
+
+            if (distanceToPlayer < aggroRange && distanceToPlayer > 1.5f)
+            {
+                rigidBody.linearVelocity = new Vector2(direction.x * movementSpeed, direction.y * movementSpeed);
+            }
+            else
+            {
+                rigidBody.linearVelocity = new Vector2(0, 0);
+                direction = Vector2.zero;
+            }
+        //}
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.collider.CompareTag("Enemy"))
+    //    {
+    //        StartCoroutine(EvadeOtherEnemy());
+    //    }
+    //}
+
+    //private IEnumerator EvadeOtherEnemy()
+    //{
+    //    isEvading = true;
+    //    rigidBody.linearVelocity = new Vector2(2, 2);
+    //    yield return new WaitForSeconds(0.5f);
+    //    isEvading = false;
+    //}
 }
