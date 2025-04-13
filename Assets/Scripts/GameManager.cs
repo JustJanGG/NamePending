b799d332388892529c48dc91c865d83be9eafcec
tlpetrick <lpetrick@stud.hs-heilbronn.de>
2025-04-13 18:49:04 +0200

    add gameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Instance")]
+    public static GameManager instance;
+
+    [Header("Lists")]
+    public GameObject[] enemyList;
+    public GameObject[] circuitList;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
