f14bc1ce783b29ec10ae25a5823d453fc04d4063
tlpetrick <lpetrick@stud.hs-heilbronn.de>
2025-04-25 01:42:55 +0200

    remove unused code

--- Assets/Scripts/Circuit/Blue/BlueCircuit.cs
+++ Assets/Scripts/Circuit/Blue/BlueCircuit.cs
@@ -12,9 +12,6 @@ public abstract class BlueCircuit : Ability, ICircuit
     [Header("Blue Circuit Stats")]
     public float procChance;
 
-    Ability socketedInAbility;
-    List<RedCircuit> redCircuits;
-
     public bool Proc(float procCoefficient)
     {
         if (Random.Range(1, 101) <= procChance * 100)
