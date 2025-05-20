def2e2e23e615a2d074d1c29a757471fb62a9586
tlpetrick <lpetrick@stud.hs-heilbronn.de>
2025-05-20 17:44:43 +0200

    added cooldown for abilites

--- Assets/Scripts/Player/PlayerAbilityController.cs
+++ Assets/Scripts/Player/PlayerAbilityController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,45 +9,87 @@ public class PlayerAbilityController : MonoBehaviour
 
     [Header("InteractionStats")]
     private float pickupRange = 1.5f;
-    
-    private void Awake()
+
+    // Cooldown tracking
+    private Dictionary<int, float> abilityCooldowns = new Dictionary<int, float>();
+
+    private void Start()
     {
         playerAbilities = GetComponentInChildren<PlayerAbilities>();
+
+        // set cooldowns to 0
+        for (int i = 0; i < playerAbilities.abilities.Count; i++)
+        {
+            abilityCooldowns[i] = 0f;
+        }
+    }
+
+    private void Update()
+    {
+        // Update cooldowns
+        foreach (var key in new List<int>(abilityCooldowns.Keys))
+        {
+            if (abilityCooldowns[key] > 0)
+            {
+                abilityCooldowns[key] -= Time.deltaTime;
+            }
+        }
+    }
+
+    private bool CanUseAbility(int abilityIndex)
+    {
+        return abilityCooldowns.ContainsKey(abilityIndex) && abilityCooldowns[abilityIndex] <= 0f;
+    }
+
+    private void SetAbilityCooldown(int abilityIndex, float cooldown)
+    {
+        if (abilityCooldowns.ContainsKey(abilityIndex))
+        {
+            abilityCooldowns[abilityIndex] = cooldown;
+        }
     }
 
     // Left Mouse Button
     public void HandleAttackInputOne(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && GameManager.instance.gameState == GameState.InGame)
+        if (ctx.performed && GameManager.instance.gameState == GameState.InGame && CanUseAbility(0))
         {
             playerAbilities.abilities[0].GetComponent<Ability>().Activate();
+            SetAbilityCooldown(0, playerAbilities.abilities[0].GetComponent<Ability>().cooldown);
+
         }
     }
 
     // Right Mouse BUtton
     public void HandleAttackInputTwo(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && GameManager.instance.gameState == GameState.InGame)
+        if (ctx.performed && GameManager.instance.gameState == GameState.InGame && CanUseAbility(1))
         {
             playerAbilities.abilities[1].GetComponent<Ability>().Activate();
+            SetAbilityCooldown(1, playerAbilities.abilities[1].GetComponent<Ability>().cooldown);
+
         }
     }
 
     // Shift
     public void HandleAttackInputThree(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && GameManager.instance.gameState == GameState.InGame)
+        if (ctx.performed && GameManager.instance.gameState == GameState.InGame && CanUseAbility(2))
         {
             playerAbilities.abilities[2].GetComponent<Ability>().Activate();
+            SetAbilityCooldown(2, playerAbilities.abilities[2].GetComponent<Ability>().cooldown);
+
         }
     }
 
     // R
     public void HandleAttackInputFour(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && GameManager.instance.gameState == GameState.InGame)
+        if (ctx.performed && GameManager.instance.gameState == GameState.InGame && CanUseAbility(3))
         {
             playerAbilities.abilities[3].GetComponent<Ability>().Activate();
+            SetAbilityCooldown(3, playerAbilities.abilities[3].GetComponent<Ability>().cooldown);
+
         }
     }
 
