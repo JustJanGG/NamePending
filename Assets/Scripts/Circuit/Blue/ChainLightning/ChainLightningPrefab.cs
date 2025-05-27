47d975aa69daab1ec0fb309077fba5d8607d4808
JustJanGG <jan.haufer@gmail.com>
2025-05-27 15:51:04 +0200

    chain lightning logic

--- Assets/Scripts/Circuit/Blue/ChainLightning/ChainLightningPrefab.cs
+++ Assets/Scripts/Circuit/Blue/ChainLightning/ChainLightningPrefab.cs
@@ -5,4 +5,24 @@ public class ChainLightningPrefab : AbilityPrefab, IBlueCircuitPrefab
 {
     public List<BlueCircuit> reducedList { get; set; }
     public Dictionary<DamageType, float> damage { get; set; }
+    private void Start()
+    {
+        
+    }
+    private GameObject FindClosestEnemy(Transform startPoint, float range)
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closestEnemy = GameObject.FindGameObjectWithTag("Enemy");
+        float closestDistance = Mathf.Infinity;
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy != null
+                && Vector3.Distance(closestEnemy.transform.position, startPoint.position) < closestDistance 
+                && Vector3.Distance(closestEnemy.transform.position, startPoint.position) <= range)
+            {
+                closestEnemy = enemy;
+            }
+        }
+        return closestEnemy;
+    }
 }
