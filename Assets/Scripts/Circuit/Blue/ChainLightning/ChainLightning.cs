31159807c7b68950fc2f8c7dad232b64384d6513
JustJanGG <jan.haufer@gmail.com>
2025-05-30 18:25:30 +0200

    implemented chain lightning damage logic and added sprite

--- Assets/Scripts/Circuit/Blue/ChainLightning/ChainLightning.cs
+++ Assets/Scripts/Circuit/Blue/ChainLightning/ChainLightning.cs
@@ -5,13 +5,22 @@ public class ChainLightning : BlueCircuit
 {
     public Ability socketedAbility { get; set; }
     public List<RedCircuit> redCircuits { get; set; }
+    public int chainCount;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        circuitName = "Chain Lighting";
+        circuitType = CircuitType.Blue;
+    }
     public override void Activate(GameObject enemy, List<BlueCircuit> blueCircuits, Dictionary<DamageType, float> damage)
     {
-        GameObject chainLightning = Instantiate(abilityPrefab, GameObject.FindGameObjectWithTag("Player").transform.position, Quaternion.identity);
+        GameObject chainLightning = Instantiate(abilityPrefab, enemy.transform.position, Quaternion.identity);
         ChainLightningPrefab chainLightningPrefab = chainLightning.GetComponent<ChainLightningPrefab>();
 
         ((IBlueCircuitPrefab)chainLightningPrefab).PassDamage(damage);
         chainLightningPrefab.prefabOf = this.gameObject;
         ((IBlueCircuitPrefab)chainLightningPrefab).PassList(blueCircuits);
+        chainLightningPrefab.lastEnemyHit = enemy;
+        chainLightningPrefab.chainCount = this.chainCount;
     }
 }
--- Assets/Scripts/Circuit/Blue/ChainLightning/ChainLightningPrefab.cs
+++ Assets/Scripts/Circuit/Blue/ChainLightning/ChainLightningPrefab.cs
@@ -5,24 +5,54 @@ public class ChainLightningPrefab : AbilityPrefab, IBlueCircuitPrefab
 {
     public List<BlueCircuit> reducedList { get; set; }
     public Dictionary<DamageType, float> damage { get; set; }
+    public float chainRange = 1000f;
+    //public GameObject initialEnemy;
+    private List<GameObject> alreadyHit;
+    public int chainCount;
+    public GameObject lastEnemyHit;
     private void Start()
     {
-        
+        Destroy(gameObject, prefabOf.GetComponent<Ability>().lifetime);
+        prefabOf.GetComponent<BlueCircuit>().Hit(lastEnemyHit, reducedList, damage);
+        alreadyHit = new List<GameObject>();
+        alreadyHit.Add(lastEnemyHit);
+        GameObject closestEnemy;
+        for (int i = 0; i < chainCount; i++)
+        {
+            closestEnemy = FindClosestEnemy(lastEnemyHit.transform, chainRange);
+            if (closestEnemy == lastEnemyHit)
+            {
+                break;
+            }
+
+            alreadyHit.Add(closestEnemy);
+            prefabOf.GetComponent<BlueCircuit>().Hit(closestEnemy, reducedList, damage);
+        }
     }
     private GameObject FindClosestEnemy(Transform startPoint, float range)
     {
+        //Fix no enemies found
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        GameObject closestEnemy = GameObject.FindGameObjectWithTag("Enemy");
+        GameObject closestEnemy = lastEnemyHit;
         float closestDistance = Mathf.Infinity;
         foreach (GameObject enemy in enemies)
         {
+            float distance = Vector3.Distance(lastEnemyHit.transform.position, enemy.transform.position);
             if (enemy != null
-                && Vector3.Distance(closestEnemy.transform.position, startPoint.position) < closestDistance 
-                && Vector3.Distance(closestEnemy.transform.position, startPoint.position) <= range)
+                && distance < closestDistance 
+                && distance <= range
+                && !alreadyHit.Contains(enemy))
             {
+                closestDistance = distance;
                 closestEnemy = enemy;
             }
         }
+        //if(closestEnemy == lastEnemyHit)
+        //{
+        //    return null; // No new enemy found within range
+        //}
+        //alreadyHit.Add(closestEnemy);
+        //prefabOf.GetComponent<BlueCircuit>().Hit(closestEnemy, reducedList, damage);
         return closestEnemy;
     }
 }
