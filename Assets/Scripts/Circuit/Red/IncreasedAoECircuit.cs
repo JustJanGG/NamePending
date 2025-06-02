b1125228bb23faef8dd676be71284f82cffba29a
tlpetrick <lpetrick@stud.hs-heilbronn.de>
2025-06-02 16:20:31 +0200

    added increased aoe circuit

---
+++ Assets/Scripts/Circuit/Red/IncreasedAoECircuit.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreasedAoECircuit : RedCircuit
+{
+    public IncreasedAoECircuit()
+    {
+        id = 4;
+        circuitName = "Increased AoE";
+        circuitDescription = "Increases the size of AoE abilityies by 50%.";
+        circuitType = CircuitType.Red;
+        tags = new List<Tag>();
+        tags.Add(Tag.AoE);
+    }
+    public override void ApplyRedCircuit(Ability ability)
+    {
+        if (ability.tags.Contains(Tag.AoE))
+        {
+            ability.GetComponent<AoEStats>().areaSize *= 1.5f;
+        }
+    }
+
+    public override void RemoveRedCircuit(Ability ability)
+    {
+        if (ability.tags.Contains(Tag.AoE))
+        {
+            ability.GetComponent<AoEStats>().areaSize /= 1.5f;
+        }
+    }
+}
